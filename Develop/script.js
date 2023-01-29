63fb8e9ec1b8a933780e1c09a4cf268d49e28470
Jsh0227 <Jsh0227@aol.com>
2023-01-29 18:56:34 -0500

    added array

--- Develop/script.js
+++ Develop/script.js
@@ -1,32 +1,62 @@
 // Assignment Code
 let generateBtn = document.querySelector("#generate");
   console.log(generateBtn)
+const upperCase=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+const lowerCase= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+const number=[1,2,3,4,5,6,7,8,9,0]
+const specialChars=['"','!','@','$','%','^','&','*','#','"']
 
-let password = ""
+
+
+let passwordGenerated = ""
 function generatePassword(){
+  let bowl =[]
   let passwordLength = prompt("How many characters do you want your password to have?")
   console.log(passwordLength)
-  if (passwordLength < 8 || passwordLength > 128 || isNaN (passwordLength)){
-    alert("Your password does not meet the criteria.")
-    generatePassword()
 
-  } else{
-    let upperCaseConfirm = confirm("Do you want an uppercase in your password?")
+  while (passwordLength < 8 || passwordLength > 15 || isNaN (passwordLength)){
+    alert("Your password does not meet the criteria. Please enter a number between 8 and 15")
+    passwordLength = prompt("How many characters do you want your password to have?")
   }
-  return 123
+  let confirmUpperCase = confirm("Would you like an uppercase letter in your password?")
+  console.log(confirmUpperCase)
+  if(confirmUpperCase === true){
+    bowl = bowl.concat(upperCase)
+    console.log(bowl)
+  }
+    
+   let confirmLowerCase = confirm("Would you like a lowercase letter in your password?")
+   if (confirmLowerCase === true){
+    bowl = bowl.concat(lowerCase)
+    console.log(bowl)
+   }
+  let confirmNumber = confirm("Would you like a number in your password?")
+  if (confirmNumber === true){
+    bowl = bowl.concat(number)
+    console.log(bowl)
+  }
+
+  let confirmSpecialChars = confirm("Would you like a number in your password?")
+  if (confirmSpecialChars === true){
+    bowl = bowl.concat(specialChars)
+    console.log(bowl)
+  }
+  for(var i = 0; i < passwordLength; i++){
+   passwordGenerated = passwordGenerated + (bowl[Math.floor(Math.random() * bowl.length)])
+  }
+  return passwordGenerated;
 }
+
+//generatePassword()
 // Write password to the #password input
 function writePassword() {
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
 
-
-  
   passwordText.value = password;
-
+  passwordGenerated = '';
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword)
 
 
